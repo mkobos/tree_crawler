8a37c57e8e210f8ceaf54242018af84521c643ea
Mateusz Kobos <mateusz@mkobos.com>
2011-09-27 17:30:43 +0200

    Fix problem with path to the web site page under windows

--- concurrent_tree_crawler/bin/run_sample_download_crawler.py
+++ concurrent_tree_crawler/bin/run_sample_download_crawler.py
@@ -4,6 +4,7 @@ import sys
 import os
 import os.path
 import datetime
+import urllib
 
 def set_path_to_package_root():
 	"""
@@ -26,7 +27,9 @@ def get_website_address():
 	"""
 	import concurrent_tree_crawler
 	package_path = os.path.dirname(concurrent_tree_crawler.__file__)
-	return "file://{}/test/data/original_site/issues_1.html".format(package_path)
+	url = urllib.pathname2url("{}/test/data/original_site/issues_1.html".\
+		format(package_path))
+	return "file:"+url
 
 website_address = get_website_address()
 script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -38,5 +41,5 @@ sleep_time_str = ("{}:{}:{}".format(sleep_time[0].hour, sleep_time[0].minute, sl
 	"{}:{}:{}".format(sleep_time[1].hour, sleep_time[1].minute, sleep_time[1].second))
 pages_per_second_download_limit = 4
 
-os.system('python {script_dir}/sample_download_crawler.py -v -v --log_file "{dst_dir}/log.txt" --max_pages_per_second {download_limit} --daily_schedule {activity_start}-{activity_end} "{dst_dir}/state.xml" {website_address} "{dst_dir}/tmp/download" '.format(download_limit=pages_per_second_download_limit, script_dir=script_dir, dst_dir=dst_dir, activity_start=sleep_time_str[1], activity_end=sleep_time_str[0], website_address=website_address))
-print "\nNote that the information printed above about problems during tree exploration is expected. It stems from the fact that some of the pages we want to download from our testing web site are missing."
+os.system('{script_dir}/sample_download_crawler.py -v -v --log_file "{dst_dir}/log.txt" --max_pages_per_second {download_limit} --daily_schedule {activity_start}-{activity_end} "{dst_dir}/state.xml" {website_address} "{dst_dir}/tmp/download" '.format(download_limit=pages_per_second_download_limit, script_dir=script_dir, dst_dir=dst_dir, activity_start=sleep_time_str[1], activity_end=sleep_time_str[0], website_address=website_address))
+print "\nNote that if above an information about problems during tree exploration has been printed, it is expected. It stems from the fact that some of the pages we want to download from our testing web site are missing."
